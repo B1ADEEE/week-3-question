8d5681709d1f04663b72c9150a5808b1db907f06
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-02-05 09:55:01 +0000

    test commit

---
+++ PracticeForTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeForTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- question 4/Program.cs
+++ question 4/Program.cs
@@ -6,17 +6,65 @@ namespace question_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("what card do you have in notations? (eg 2d for two diomands)");
+            Console.WriteLine("what card do you have ? (eg 2d for two of diomands)");
             string userInfo = Console.ReadLine();
-            int cardNum = 0;
+            string cardNum = "";
+            string cardType = "";
 
             switch (userInfo[0])
             {
-                case:
-
+                case '2':
+                    cardNum = "two";
+                    break;
+                case '3':
+                    cardNum = "three";
+                    break;
+                case '4':
+                    cardNum = "four";
+                    break;
+                case '5':
+                    cardNum = "five";
+                    break;
+                case '6':
+                    cardNum = "six";
+                    break;
+                case '7':
+                    cardNum = "seven";
+                    break;
+                case '8':
+                    cardNum = "eight";
+                    break;
+                case '9':
+                    cardNum = "nine";
+                    break;
+                case 'q':
+                    cardNum = "Queen";
+                    break;
+                case 'a':
+                    cardNum = "ace";
+                    break;
+                case 'K':
+                    cardNum = "king";
+                    break;
+                case 'j':
+                    cardNum = "jack";
+                    break;
+                default:
+                    cardNum = "ten";
+                    break;
+            }
+            switch (userInfo[1])
+            {
+                case 'd':
+                    cardType = " of diomands";
+                    break;
+                case 'h':
+                    cardType = " of hearts";
+                    break;
+                case 's':
+                    cardType = ""
                 default:
                     break;
-
             }
         }
     }
