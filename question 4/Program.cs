e5d35679fe8594350a2345bce97171a403039817
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-02-05 09:08:32 +0000

    Add project files.

---
+++ question 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace question_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what card do you have in notations? (eg 2d for two diomands)");
+            string userInfo = Console.ReadLine();
+            int cardNum = 0;
+
+            switch (userInfo[0])
+            {
+                case:
+
+                default:
+                    break;
+
+            }
+        }
+    }
+}
---
+++ week 3 question/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace week_3_question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what score did you get?");
+            int gameScore = int.Parse(Console.ReadLine());
+            string gameQuote = "";
+
+            switch (gameScore / 10)
+            {
+                case 10:
+                case 9:
+                case 8:
+                    gameQuote = "Awsome dude";
+                    break;
+                case 7:
+                    gameQuote = "Your good dude";
+                    break;
+                case 6:
+                    gameQuote = "Some potential here dude";
+                    break;
+                case 5:
+                    gameQuote = "back to the training ground dude";
+                    break;
+                case 4:
+                case 3:
+                case 2:
+                case 1:
+                case 0:
+                    gameQuote = "dont quit your day job";
+                    break;
+            }
+            Console.WriteLine(gameQuote);
+        }
+    }
+}
